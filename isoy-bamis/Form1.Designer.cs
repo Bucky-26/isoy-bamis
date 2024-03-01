fb7f7c526a07afa66f5da3e9babd3b80f38d8de2
Bucky <75648240+Bucky-26@users.noreply.github.com>
2024-03-01 16:27:58 +0800

    v1

--- isoy-bamis/Form1.Designer.cs
+++ isoy-bamis/Form1.Designer.cs
@@ -259,10 +259,11 @@ namespace isoy_bamis
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(69, 121);
+            this.label1.Location = new System.Drawing.Point(77, 149);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "LORIM IPSUM";
             this.label1.Click += new System.EventHandler(this.label1_Click);
